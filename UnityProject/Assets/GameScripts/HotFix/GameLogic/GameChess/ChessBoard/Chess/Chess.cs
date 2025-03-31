5d6312aeeac815cec2b7d2d671c9a1b2a329c5b9
shenjunjie <shenjunjie@qq.com>
2025-03-31 20:53:37 +0800

    组件微调

--- UnityProject/Assets/GameScripts/HotFix/GameLogic/GameChess/ChessBoard/Chess/Chess.cs
+++ UnityProject/Assets/GameScripts/HotFix/GameLogic/GameChess/ChessBoard/Chess/Chess.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using GameConfig;
 using TEngine;
 using UnityEngine;
 
@@ -7,6 +8,27 @@ namespace GameLogic
 {
     public class Chess : Entity
     {
-       
+        protected Chess_ConfigItem _chessConfigItem;
+
+        public virtual void UpdateChessType()
+        {
+            
+        }
+
+        public virtual void SetChessMono()
+        {
+            
+        }
+
+        public virtual void OnChessClick()
+        {
+            
+        }
+
+        public static void Create(Chess_ConfigItem _chessConfigItem)
+        {
+            Chess chess = new Chess();
+            
+        }
     }
 }
